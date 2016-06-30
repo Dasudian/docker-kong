646a0b431bb89a7f1c43ee1a9b6b766cd9a5cb90
Mengz <mz@dasudian.com>
2016-06-30 18:33:11 +0800

    add some default configuration
    change the cluster listen port default to 7956

--- setup.sh
+++ setup.sh
@@ -2,5 +2,21 @@
 
 # Setting up the proper database
 if [ -n "$DATABASE" ]; then
-  echo -e '\ndatabase: "'$DATABASE'"' >> /etc/kong/kong.yml
+  sed -ri "s|^database: .*|database: $DATABASE|" $KONG_CONFIG
+fi
+
+if [ -n "$PG_HOST" ]; then
+  sed -ri "s|^  host: .*|  host: '$PG_HOST'|" $KONG_CONFIG
+fi
+
+if [ -n "$PG_DB" ]; then
+  sed -ri "s|^  database: .*|  database: $PG_DB|" $KONG_CONFIG
+fi
+
+if [ -n "$PG_USER" ]; then
+  sed -ri "s|^  user: .*|  user: $PG_USER|" $KONG_CONFIG
+fi
+
+if [ -n "$PG_PASSWD" ]; then
+  sed -ri "s|^  password: .*|  password: $PG_PASSWD|" $KONG_CONFIG
 fi
